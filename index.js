896e3abde9a717ff1feb258ff6b7465269f083cb
kolism <14796164+kolism@users.noreply.github.com>
2022-10-15 16:36:12 -0400

    Module2

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 4000;
+
+const mongoDB = "mongodb+srv://mardation:1marc1@cluster0.obalbih.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
